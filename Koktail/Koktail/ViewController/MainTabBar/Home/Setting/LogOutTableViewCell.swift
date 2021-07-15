81db47f23bb7670ac9e88ac51de95a3726c69fa8
최승명 <710csm@naver.com>
2021-07-15 10:25:59 +0900

    feat: 로그아웃 기능 추가

--- Koktail/Koktail/ViewController/MainTabBar/Home/Setting/LogOutTableViewCell.swift
+++ Koktail/Koktail/ViewController/MainTabBar/Home/Setting/LogOutTableViewCell.swift
@@ -8,6 +8,7 @@
 import UIKit
 import RxCocoa
 import RxSwift
+import Firebase
 
 class LogOutTableViewCell: UITableViewCell {
 
@@ -22,15 +23,24 @@ class LogOutTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        setButton()
+        setUI()
+    }
+    
+    private func setButton() {
         logoutButton.rx.tap.bind {
             self.logoutAlert()
         }.disposed(by: disposeBag)
     }
     
+    private func setUI(){
+        
+    }
+    
     private func logoutAlert() {
         let alert = UIAlertController(title: "안내", message: "로그아웃 하시겠습니까?", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "확인", style: .default) { _ in
-            print("logout")
+            self.requestLogOut()
         }
         let cancelAction = UIAlertAction(title: "취소", style: .cancel)
         
@@ -38,5 +48,16 @@ class LogOutTableViewCell: UITableViewCell {
         alert.addAction(cancelAction)
         
         settingViewController?.present(alert, animated: true)
+    }
+    
+    private func requestLogOut() {
+        do {
+            try Auth.auth().signOut()
+            
+            guard let window = settingViewController?.view.window else { return }
+            window.switchRootViewController(LoginPageViewController())
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
