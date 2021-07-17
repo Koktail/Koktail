0c473b8c9c9d6da42e22e1fcb8c3da93406add9d
최승명 <710csm@naver.com>
2021-07-17 23:56:05 +0900

    feat: 구글 맵, 플레이스 설치

--- Koktail/Koktail/ViewController/MainTabBar/Map/MapViewController.swift
+++ Koktail/Koktail/ViewController/MainTabBar/Map/MapViewController.swift
@@ -6,10 +6,35 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var map: GMSMapView!
+    private var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    private func setLocationInfo() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
+        // 앱이 실행될 때 위치 추적 권한 요청
+        locationManager.requestWhenInUseAuthorization()
+        // 배터리에 맞게 권장되는 최적의 정확도
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // 위치 업데이트
+        locationManager.startUpdatingLocation()
+        
+        // 위, 경도 가져오기
+        let coor = locationManager.location?.coordinate
+        
+        let latitude = (coor?.latitude ?? 37.566508) as Double
+        let longitude = (coor?.longitude ?? 126.977945) as Double
     }
 }
