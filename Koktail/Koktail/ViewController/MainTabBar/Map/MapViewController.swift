3be2e12b9bb37afe9c047e7c38ab88911a1deb25
최승명 <710csm@naver.com>
2021-07-18 17:49:34 +0900

    feat: 현재 위치정보를 받아 지도에 표시

--- Koktail/Koktail/ViewController/MainTabBar/Map/MapViewController.swift
+++ Koktail/Koktail/ViewController/MainTabBar/Map/MapViewController.swift
@@ -7,34 +7,149 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
 class MapViewController: UIViewController {
-
+    
     @IBOutlet weak var map: GMSMapView!
-    private var locationManager: CLLocationManager!
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation?
+    
+    var placesClient: GMSPlacesClient!
+    var preciseLocationZoomLevel: Float = 15.0
+    var approximateLocationZoomLevel: Float = 10.0
+    
+    // An array to hold the list of likely places.
+    var likelyPlaces: [GMSPlace] = []
+    
+    // The currently selected place.
+    var selectedPlace: GMSPlace?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setLocationInfo()
+    }
+    
+    private func setLocationInfo() {
+        // Initialize the location manager.
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+        placesClient = GMSPlacesClient.shared()
+        
+        // A default location to use when location permission is not granted.
+        let defaultLocation = CLLocation(latitude: 33, longitude: 128)
+        
+        // Create a map.
+        let zoomLevel = locationManager.accuracyAuthorization == .fullAccuracy ?
+            preciseLocationZoomLevel : approximateLocationZoomLevel
+        let camera = GMSCameraPosition.camera(
+            withLatitude: defaultLocation.coordinate.latitude,
+            longitude: defaultLocation.coordinate.longitude,
+            zoom: zoomLevel
+        )
+        
+        map.camera = camera
+        map.settings.myLocationButton = true
+        map.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        map.isMyLocationEnabled = true
+        
+        // Add the map to the view, hide it until we've got a location update.
+        map.isHidden = true
+        
+        listLikelyPlaces()
+    }
+    
+    // Populate the array with the list of likely places.
+    private func listLikelyPlaces() {
+        // Clean up from previous sessions.
+        likelyPlaces.removeAll()
+        
+        let placeFields: GMSPlaceField = [.name, .coordinate]
+        placesClient.findPlaceLikelihoodsFromCurrentLocation(withPlaceFields: placeFields) { (placeLikelihoods, error) in
+            guard error == nil else {
+                print("Current Place error: \(error!.localizedDescription)")
+                return
+            }
+            
+            guard let placeLikelihoods = placeLikelihoods else {
+                print("No places found.")
+                return
+            }
+            
+            // Get likely places and add to the list.
+            for likelihood in placeLikelihoods {
+                let place = likelihood.place
+                self.likelyPlaces.append(place)
+            }
+        }
     }
 }
 
 extension MapViewController: CLLocationManagerDelegate {
     
-    private func setLocationInfo() {
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
+    // Handle incoming location events.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        print("Location: \(location)")
         
-        // 앱이 실행될 때 위치 추적 권한 요청
-        locationManager.requestWhenInUseAuthorization()
-        // 배터리에 맞게 권장되는 최적의 정확도
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        // 위치 업데이트
-        locationManager.startUpdatingLocation()
+        let zoomLevel = locationManager.accuracyAuthorization == .fullAccuracy ?
+            preciseLocationZoomLevel : approximateLocationZoomLevel
+        let camera = GMSCameraPosition.camera(
+            withLatitude: location.coordinate.latitude,
+            longitude: location.coordinate.longitude,
+            zoom: zoomLevel
+        )
         
-        // 위, 경도 가져오기
-        let coor = locationManager.location?.coordinate
+        if map.isHidden {
+            map.isHidden = false
+            map.camera = camera
+        } else {
+            map.animate(to: camera)
+        }
         
-        let latitude = (coor?.latitude ?? 37.566508) as Double
-        let longitude = (coor?.longitude ?? 126.977945) as Double
+        listLikelyPlaces()
+    }
+    
+    // Handle authorization for the location manager.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // Check accuracy authorization
+        let accuracy = manager.accuracyAuthorization
+        switch accuracy {
+        case .fullAccuracy:
+            print("Location accuracy is precise.")
+        case .reducedAccuracy:
+            print("Location accuracy is not precise.")
+        @unknown default:
+            fatalError()
+        }
+        
+        // Handle authorization status
+        switch status {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+            // Display the map using the default location.
+            map.isHidden = false
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    // Handle location manager errors.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Error: \(error)")
     }
 }
