6b302e17bbbf4bc89816421040e5cf60f5424027
졍연희 <anny7867@naver.com>
2021-07-26 21:44:08 +0900

    feat: 웹으로 카카오 로그인 기능 추가

---
+++ Koktail/Koktail/ViewController/Login/GetEmailViewController.swift
@@ -0,0 +1,122 @@
+//
+//  GetEmailViewController.swift
+//  Koktail
+//
+//  Created by 정연희 on 2021/07/22.
+//
+
+import UIKit
+import SkyFloatingLabelTextField
+
+import FirebaseAuth
+import KakaoSDKUser
+
+class GetEmailViewController: UIViewController {
+
+    // MARK: - Properties
+    var color: String = "system"
+    
+    @IBOutlet weak var emailTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    let tabBarViewController = MainTabBarController()
+    
+    // MARK: - Action
+    @objc func emailTextFieldDidChange(_ textfield: UITextField) {
+        if let text = textfield.text {
+            if let floatingLabelTextField = textfield as? SkyFloatingLabelTextField {
+                if text.count < 3 || !text.contains("@") {
+                    floatingLabelTextField.errorMessage = "잘못된 이메일 형식입니다."
+                } else {
+                    floatingLabelTextField.errorMessage = ""
+                }
+            }
+        }
+    }
+    
+    /// 카카오톡으로 로그인한 정보를 파이어베이스에 등록
+    @objc func okEvent() {
+        if let email = emailTextField.text {
+            if isValidEmail(email: email) {
+                UserApi.shared.me { [self] kakaoUser, error in
+                    if let error = error {
+                        print("< KAKAO: user loading failed >")
+                        print(error.localizedDescription)
+                    } else {
+                        guard let password = kakaoUser?.id else {
+                            return
+                        }
+                        
+                        Auth.auth().createUser(withEmail: email,
+                                               password: String(describing: password)) { _, error in
+                            if let error = error {
+                                print("< FIREBASE: signUp failed >")
+                                print(error.localizedDescription)
+                                
+                                Auth.auth().signIn(withEmail: email,
+                                                   password: String(describing: password)) { _, signInError in
+                                    if let signInError = signInError {
+                                        print("< FIREBASE: signIn failed >")
+                                        print(signInError.localizedDescription)
+                                    } else {
+                                        print(" < FIREBASE: signIn success >")
+                                    }
+                                }
+                            } else {
+                                print("< FIREBASE: signup success >")
+                            }
+                        }
+                        
+                        UserDefaultsManager.userId = email
+                        self.view.window?.switchRootViewController(self.tabBarViewController)
+                    }
+                }
+            }
+        }
+    }
+    
+    // MARK: - Override Method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initProperties()
+        
+        emailTextField.addTarget(self, action: #selector(emailTextFieldDidChange(_:)), for: .editingChanged)
+        okButton.addTarget(self, action: #selector(okEvent), for: .touchUpInside)
+    }
+    
+    // MARK: - Custom Method
+    func isValidEmail(email: String) -> Bool {
+        if email == "" {
+            return false
+        }
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        
+        return emailTest.evaluate(with: email)
+    }
+    
+    func initProperties() {
+        okButton.layer.cornerRadius = 5
+        cancelButton.layer.cornerRadius = 5
+        okButton.backgroundColor = UIColor(hex: color)
+        cancelButton.backgroundColor = UIColor(hex: color)
+        
+        emailTextField.title = "Email"
+        
+        emailTextField.lineColor = .lightGray
+        
+        if color != "system" {
+            emailTextField.tintColor = UIColor(hex: color)
+            emailTextField.selectedTitleColor = UIColor(hex: color)
+            emailTextField.selectedLineColor = UIColor(hex: color)
+        }
+        
+        emailTextField.errorColor = .red
+
+        emailTextField.lineHeight = 1.0
+        emailTextField.selectedLineHeight = 2.0
+    }
+}
